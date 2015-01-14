e164c0cddaf52811429dba73148f8dd5731d5a7a
Emmanuel BALLERY <emmanuel.ballery@gmail.com>
2015-01-15 00:58:07 +0100

    Fix bundle configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,9 +23,9 @@ class Configuration implements ConfigurationInterface
         $children = $rootNode->addDefaultsIfNotSet()->children();
         $children->scalarNode('domain')->defaultValue('messages')->info('Default translation domain')->example('messages');
         $children->scalarNode('locale')->defaultValue('%locale%')->info('Default translation locale')->example('%locale%');
-        $children->booleanNode('useRouteAsClass')->defaultFalse()->info('Add route name as class when generating a link')->example('true');
-        $children->booleanNode('replaceUnderscore')->defaultTrue()->info('Replace underscores by point in route names when generating a translation')->example('false');
-        $children->scalarNode('trackSelectedLinks')->defaultNull()->info('Selected links class')->example('active');
+        $children->booleanNode('use_route_as_class')->defaultFalse()->info('Add route name as class when generating a link')->example('true');
+        $children->booleanNode('replace_underscore')->defaultTrue()->info('Replace underscores by point in route names when generating a translation')->example('false');
+        $children->scalarNode('track_selected_links')->defaultNull()->info('Selected links class')->example('active');
         $children->scalarNode('prefix')->defaultValue('page.')->info('Translations prefix')->example('page.');
 
         return $treeBuilder;
--- DependencyInjection/EBTranslationExtension.php
+++ DependencyInjection/EBTranslationExtension.php
@@ -21,7 +21,12 @@ class EBTranslationExtension extends Extension
     {
         // Load configuration
         $conf = $this->processConfiguration(new Configuration(), $configs);
-        $container->setParameter('eb_translation.translation.conf', $conf);
+        $container->setParameter('eb_translation.translation.domain', $conf['domain']);
+        $container->setParameter('eb_translation.translation.locale', $conf['locale']);
+        $container->setParameter('eb_translation.translation.use_route_as_class', $conf['use_route_as_class']);
+        $container->setParameter('eb_translation.translation.replace_underscore', $conf['replace_underscore']);
+        $container->setParameter('eb_translation.translation.track_selected_links', $conf['track_selected_links']);
+        $container->setParameter('eb_translation.translation.prefix', $conf['prefix']);
 
         // Load services
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
